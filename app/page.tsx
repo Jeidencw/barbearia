70a1d9a2f43d912d335a5902bb579c53fed1c56f
Henrique <henriquecw22@gmail.com>
2025-09-23 21:50:35 -0300

    feat: add header component and banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,33 @@
-import { Button } from "@/app/ui/button";
+import { SearchIcon } from "lucide-react";
+import Header from "./_components/Header";
+import { Input } from "./_components/ui/input";
+import { Button } from "./ui/button";
+import Image from "next/image";
 
 const Home = () => {
     return (
         <div className="">
-            <h1>Home</h1>
-            <Button>Botão</Button>
+            <Header />
+            <div className="p-5">
+                <h2 className="text-xl font-bold">Olá, Henrique</h2>
+                <p>Segunda-feira, 23 de Setembro</p>
+
+                <div className="mt-6 flex items-center gap-2">
+                    <Input placeholder="Faça sua busca..." />
+                    <Button>
+                        <SearchIcon />
+                    </Button>
+                </div>
+
+                <div className="relative mt-6 h-[150px] w-full">
+                    <Image
+                        src="/banner-01.png"
+                        fill
+                        className="rounded-xl object-cover"
+                        alt="agende com os melhores com fsw barber"
+                    />
+                </div>
+            </div>
         </div>
     );
 };
