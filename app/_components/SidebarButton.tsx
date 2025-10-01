00e658975741447be2cb1ad9e3c090718f2fc163
Henrique <henriquecw22@gmail.com>
2025-09-30 21:12:49 -0300

    chore: add next auth setup

--- app/_components/SidebarButton.tsx
+++ app/_components/SidebarButton.tsx
@@ -1,5 +1,11 @@
 import { Button } from "../ui/button";
-import { CalendarIcon, HomeIcon, LogOutIcon, MenuIcon } from "lucide-react";
+import {
+    CalendarIcon,
+    HomeIcon,
+    LogInIcon,
+    LogOutIcon,
+    MenuIcon,
+} from "lucide-react";
 import {
     Sheet,
     SheetClose,
@@ -13,6 +19,14 @@ import { Avatar, AvatarImage } from "./ui/avatar";
 import Link from "next/link";
 import { quickSearchOptions } from "../_constants/search";
 import Image from "next/image";
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "./ui/dialog";
 
 const SidebarButton = () => {
     return (
@@ -28,20 +42,57 @@ const SidebarButton = () => {
                     <SheetTitle>Menu</SheetTitle>
                 </SheetHeader>
 
-                <div className="flex items-center gap-3 border-b border-solid py-5">
-                    <Avatar>
+                <div className="flex items-center justify-between gap-3 border-b border-solid py-5">
+                    <h2 className="font-bold">Olá, faça seu login!</h2>
+
+                    <Dialog>
+                        <DialogTrigger asChild>
+                            <Button size="icon">
+                                <LogInIcon />
+                            </Button>
+                        </DialogTrigger>
+                        <DialogContent className="w-[90%] rounded-sm">
+                            <DialogHeader>
+                                <DialogTitle>
+                                    Faça seu login na plataforma
+                                </DialogTitle>
+                                <DialogDescription>
+                                    Conecte-se usando sua conta do Google.
+                                </DialogDescription>
+                            </DialogHeader>
+
+                            <Button
+                                variant="outline"
+                                className="gap-1 font-bold"
+                            >
+                                <Image
+                                    src="/Google.svg"
+                                    width={18}
+                                    height={18}
+                                    alt="fazer login com o google"
+                                />
+                                Google
+                            </Button>
+                        </DialogContent>
+                    </Dialog>
+
+                    {/* <Avatar>
                         <AvatarImage src="https://plus.unsplash.com/premium_photo-1671656349218-5218444643d8?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
                     </Avatar>
 
                     <div>
                         <p className="font-bold">Henrique de Camargo</p>
                         <p className="text-xs">henrique@email.com</p>
-                    </div>
+                    </div> */}
                 </div>
 
                 <div className="flex flex-col gap-2 border-b border-solid py-4">
                     <SheetClose asChild>
-                        <Button className="justify-start gap-2" asChild>
+                        <Button
+                            className="justify-start gap-2"
+                            asChild
+                            variant="ghost"
+                        >
                             <Link href="/">
                                 <HomeIcon size={18} />
                                 Início
